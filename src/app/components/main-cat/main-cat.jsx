ed8444b5a38c1e34b6722a68853bb85f93ade7e3
Kuroikuma <odiseoj676@gmail.com>
2022-02-01 16:14:38 -0600

    feat(Root):terminando la home

--- src/app/components/main-cat/main-cat.jsx
+++ src/app/components/main-cat/main-cat.jsx
@@ -2,7 +2,6 @@ import style from './main-cat.module.css'
 import { Logo } from './logo'
 import { Main } from './main'
 import { Discover } from '../discover/discover'
-import { Information } from '../information/information'
 
 export const MainCat = () => {
   return (
@@ -11,7 +10,6 @@ export const MainCat = () => {
         <Logo />
         <Main />
         <Discover />
-        <Information />
       </div>
     </>
   )
