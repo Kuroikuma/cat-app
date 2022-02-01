bd2a089bbdbb51a7dae54450c39ce3a5df5a266f
Kuroikuma <odiseoj676@gmail.com>
2022-02-01 16:07:20 -0600

    feat(Root):Terminando el footer

--- src/app/page/home/home.jsx
+++ src/app/page/home/home.jsx
@@ -1,11 +1,15 @@
 import style from './home.module.css'
 import { MainCat } from '../../components/main-cat/main-cat'
+import { Information } from '../../components/information/information'
+import { Footer } from '../../components/footer/footer'
 
 export const Home = () => {
   return (
     <>
       <div className={style.container}>
         <MainCat />
+        <Information />
+        <Footer />
       </div>
     </>
   )
