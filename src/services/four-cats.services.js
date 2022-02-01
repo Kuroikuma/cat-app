50d34d038803dca047b3853f0bc96e799865abf8
Kuroikuma <odiseoj676@gmail.com>
2022-01-31 22:44:52 -0600

    feat(Root):agregando datos para el compondente discover

---
+++ src/services/four-cats.services.js
@@ -0,0 +1,22 @@
+export const fourCats = [
+  {
+    name: 'Siamese',
+    breed_id: 'siam',
+    url: 'https://cdn2.thecatapi.com/images/DFHMMPNcD.jpg',
+  },
+  {
+    name: 'Russian Blue',
+    breed_id: 'rblu',
+    url: 'https://cdn2.thecatapi.com/images/kmvetZsyr.jpg',
+  },
+  {
+    name: 'Siberian',
+    breed_id: 'sibe',
+    url: 'https://cdn2.thecatapi.com/images/mcHWg83Yj.jpg',
+  },
+  {
+    name: 'American Bobtail',
+    breed_id: 'abob',
+    url: 'https://cdn2.thecatapi.com/images/gVrhv_yAY.jpg',
+  },
+]
